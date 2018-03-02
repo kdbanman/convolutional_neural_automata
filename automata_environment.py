ff6d5452e9b253f81d980b0c38f440d1c4153cbc
Kirby Banman <kirby.b@getjobber.com>
2018-03-02 16:32:05 -0700

    decouple automata environment and ruleset

---
+++ automata_environment.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+class AutomataEnvironment:
+    
+    def __init__(self, rules, grid_height, grid_width):
+        self.rules = rules
+
+        self.grid_height = grid_height
+        self.grid_width = grid_width
+        
+        self.current_grid = np.empty((grid_height, grid_width))
+        self.previous_grid = np.empty((grid_height, grid_width))
+
+    def copy_state(self, source_environment):
+        self.grid_height = source_environment.grid_height
+        self.grid_width = source_environment.grid_width
+
+        self.current_grid = np.copy(source_environment.current_grid)
+        self.previous_grid = np.copy(source_environment.previous_grid)
+        
+    def set_state_from_strings(self, string_array, dead_cell = "_", live_cell = "X"):
+        self.grid_height = len(string_array)
+        self.grid_width = len(string_array[0])
+        
+        serialized_integer_array = ",".join("".join(string_array)).replace(dead_cell, "0").replace(live_cell, "1")
+        self.current_grid = np.fromstring(serialized_integer_array, dtype=float, sep=",").reshape(self.grid_height, self.grid_width)
+        self.previous_grid = np.empty((self.grid_height, self.grid_width))
+        
+    def randomize_state(self, live_probability = 0.2):
+        self.current_grid = np.random.choice(
+            [1, 0],
+            self.grid_height * self.grid_width,
+            p=[live_probability, 1 - live_probability]
+        ).reshape(self.grid_height, self.grid_width)
+
+    def iterate(self):
+        # Triangle swap the grid references so that current grid is now the previous grid.
+        tmp_grid = self.previous_grid
+        self.previous_grid = self.current_grid
+        self.current_grid = tmp_grid
+
+        self.rules.mutate_current_grid(self.previous_grid, self.current_grid)
---
+++ conways_rules.py
@@ -0,0 +1,28 @@
+class ConwaysRules:
+    def mutate_current_grid(self, previous_grid, current_grid):
+        grid_height = current_grid.shape[0]
+        grid_width = current_grid.shape[1]
+
+        # Overwrite the current grid based on the previous grid contents.
+        for row in range(grid_height):
+            for col in range(grid_width):
+                live_neighbours = (
+                    previous_grid[row, (col - 1) % grid_width] +
+                    previous_grid[row, (col + 1) % grid_width] +
+                    previous_grid[(row - 1) % grid_height, col] +
+                    previous_grid[(row + 1) % grid_height, col] +
+                    previous_grid[(row - 1) % grid_height, (col - 1) % grid_width] +
+                    previous_grid[(row - 1) % grid_height, (col + 1) % grid_width] +
+                    previous_grid[(row + 1) % grid_height, (col - 1) % grid_width] +
+                    previous_grid[(row + 1) % grid_height, (col + 1) % grid_width]
+                )
+                if previous_grid[row, col] == 1:
+                    if live_neighbours == 2 or live_neighbours == 3:
+                        current_grid[row, col] = 1
+                    else:
+                        current_grid[row, col] = 0
+                else:
+                    if live_neighbours == 3:
+                        current_grid[row, col] = 1
+                    else:
+                        current_grid[row, col] = 0
