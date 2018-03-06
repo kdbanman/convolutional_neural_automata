c113d6ddc43c319848826bb7385ffc02216e5c0b
Kirby Banman <kirby.b@getjobber.com>
2018-03-06 09:12:44 -0700

    add option to round neural automata

--- neural_rules.py
+++ neural_rules.py
@@ -3,8 +3,9 @@ import numpy as np
 
 class NeuralRules:
 
-    def __init__(self, conv_net):
+    def __init__(self, conv_net, round_to_integers = False):
         self.conv_net = conv_net
+        self.round_to_integers = round_to_integers
 
     def mutate_current_grid(self, previous_grid, current_grid):
         previous_grid_tensor = torch.autograd.Variable(torch.from_numpy(previous_grid))
@@ -13,3 +14,6 @@ class NeuralRules:
         # Get numpy array back and drop batch and channel placeholder dimensions.
         computed_grid = computed_tensor.data.numpy().reshape(current_grid.shape)
         np.copyto(current_grid, computed_grid)
+
+        if self.round_to_integers:
+            np.round(current_grid)
