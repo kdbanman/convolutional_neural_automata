77ce1c01168d371f37fbc0c29d6d3d855c775b9f
Kirby Banman <kirby.b@getjobber.com>
2018-03-18 17:09:41 -0600

    readably implement @thearn's fft conway's rules

--- conways_rules.py
+++ conways_rules.py
@@ -5,32 +5,41 @@
 # Redistributable and modifiable under the terms of GNU GPLv3.
 # See LICENSE.txt or <http://www.gnu.org/licenses/>
 
+# Conway's rules by numpy fft taken from here:
+# https://github.com/thearn/game-of-life
+
+import numpy as np
+from numpy.fft import fft2, ifft2
 
 class ConwaysRules:
-    def mutate_current_grid(self, previous_grid, current_grid):
-        grid_height = current_grid.shape[0]
-        grid_width = current_grid.shape[1]
 
-        # Overwrite the current grid based on the previous grid contents.
-        for row in range(grid_height):
-            for col in range(grid_width):
-                live_neighbours = (
-                    previous_grid[row, (col - 1) % grid_width] +
-                    previous_grid[row, (col + 1) % grid_width] +
-                    previous_grid[(row - 1) % grid_height, col] +
-                    previous_grid[(row + 1) % grid_height, col] +
-                    previous_grid[(row - 1) % grid_height, (col - 1) % grid_width] +
-                    previous_grid[(row - 1) % grid_height, (col + 1) % grid_width] +
-                    previous_grid[(row + 1) % grid_height, (col - 1) % grid_width] +
-                    previous_grid[(row + 1) % grid_height, (col + 1) % grid_width]
-                )
-                if previous_grid[row, col] == 1:
-                    if live_neighbours == 2 or live_neighbours == 3:
-                        current_grid[row, col] = 1
-                    else:
-                        current_grid[row, col] = 0
-                else:
-                    if live_neighbours == 3:
-                        current_grid[row, col] = 1
-                    else:
-                        current_grid[row, col] = 0
+    def fft_convolve2d(self, previous_grid, kernel):
+        fourier_transformed = fft2(previous_grid)
+        transformed_by_flipped_kernel = fft2(np.flipud(np.fliplr(kernel)))
+
+        transformed_height, transformed_width = fourier_transformed.shape
+        inverse_transformed = np.real(ifft2(fourier_transformed*transformed_by_flipped_kernel))
+
+        vertically_translated_inverse = np.roll(inverse_transformed, - int(transformed_height / 2) + 1, axis=0)
+        final_inverse = np.roll(vertically_translated_inverse, - int(transformed_width / 2) + 1, axis=1)
+
+        return final_inverse
+
+    def mutate_current_grid(self, previous_grid, current_grid):
+        grid_height, grid_width = previous_grid.shape
+
+        small_von_neuman_kernel = np.array([
+            [1, 1, 1],
+            [1, 0, 1],
+            [1, 1, 1]
+        ])
+        von_neuman_kernel = np.zeros((grid_height, grid_width))
+        von_neuman_kernel[grid_height // 2 - 1 : grid_height // 2+2, grid_width // 2 - 1 : grid_width // 2 + 2] = small_von_neuman_kernel
+
+        neighborhood_sums_grid = self.fft_convolve2d(previous_grid, von_neuman_kernel).round()
+
+        current_grid.fill(0)
+        current_grid[np.where((neighborhood_sums_grid == 2) & (previous_grid == 1))] = 1
+        current_grid[np.where((neighborhood_sums_grid == 3) & (previous_grid == 1))] = 1
+
+        current_grid[np.where((neighborhood_sums_grid == 3) & (previous_grid == 0))] = 1
